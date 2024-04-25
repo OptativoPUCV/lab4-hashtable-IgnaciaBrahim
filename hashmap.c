05c19a981e997e6529d68dfe0210391877501d78
Ignacia Brahim <137849174+IgnaciaBrahim@users.noreply.github.com>
2024-04-25 02:19:31 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -165,7 +165,7 @@ Pair * nextMap(HashMap * map) {
   }
   long copia_i = map->current;
   map->current = ((map->current) + 1);
-  while(map->current + 1 >= map->capacity && map->buckets[map->current] == NULL || map->buckets[map->current]->key == NULL)
+  while((map->current + 1) >= map->capacity || map->buckets[map->current] == NULL || map->buckets[map->current]->key == NULL)
     {
       map->current = ((map->current) + 1) % map->capacity;
       if (copia_i == map->current)
